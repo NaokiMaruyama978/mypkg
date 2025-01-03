4685b22cef5d21f256f89c0e6ffb8f85380dfe75
Naoki maruyama <s23c1135vq@s.chibakoudai.jp>
2025-01-03 21:27:45 +0900

    test.bashの修正

--- test/test.bash
+++ test/test.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-
 dir=~
-[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。      
+[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 
 cd $dir/ros2_ws
 colcon build
-source $dir/.bashrc 
-timeout 10 ros2 run mypkg talker | tee - /tmp/mypkg.log
+source $dir/.bashrc
 
+# タイムアウト時間を長めに設定
+timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep '2029年の干支は酉(とり)'
- #2033年の干支は丑(うし)
+# ログファイルの内容を検索
+cat /tmp/mypkg.log | grep -E '\s*2028年の干支は申\[さる\]'
+
