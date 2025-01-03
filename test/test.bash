40fd85c91f5e216468358066db2a7418b1e42943
Naoki maruyama <s23c1135vq@s.chibakoudai.jp>
2025-01-03 21:55:37 +0900

    talker.pyの名前zodiac_publisher.pyに変えた

--- test/test.bash
+++ test/test.bash
@@ -7,8 +7,8 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-# タイムアウト時間を長めに設定
-timeout 20 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+# タイムアウト時間を設定
+timeout 20 ros2 launch mypkg zodiac_publisher.launch.py > /tmp/mypkg.log
 
 # ログファイルの内容を検索
 cat /tmp/mypkg.log | grep -E '\s*2028年の干支は申\[さる\]'
