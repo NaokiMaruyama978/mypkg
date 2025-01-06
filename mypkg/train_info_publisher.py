f6f5a747baa47399ea8ee947c1ccc63df6a95a71
Naoki maruyama <s23c1135vq@s.chibakoudai.jp>
2025-01-07 03:55:41 +0900

    ログの修正

--- mypkg/train_info_publisher.py
+++ mypkg/train_info_publisher.py
@@ -8,18 +8,24 @@ from std_msgs.msg import String
 import requests
 import sys
 
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+import requests
+import sys
+
 class FilteredTrainInfoPublisher(Node):
     def __init__(self):
         super().__init__('filtered_train_info_publisher')
         self.publisher_ = self.create_publisher(String, 'train_delay_info', 10)
-        self.timer = self.create_timer(10.0, self.timer_callback)  # 60秒ごとに更新
+        self.timer = self.create_timer(10.0, self.timer_callback)  # 10秒ごとに更新
         self.api_url = "https://api.odpt.org/api/v4/odpt:TrainInformation"
-        self.api_key = "3o7usx306xa0q9chlckckk2xxm2jvthznu0vnk3fktuu9gdirfp3pzcecwlpagwa"  # 取得したAPIキーを設定 
+        self.api_key = "3o7usx306xa0q9chlckckk2xxm2jvthznu0vnk3fktuu9gdirfp3pzcecwlpagwa"  # 取得したAPIキーを設定
         self.target_railways = [
- 
-            "odpt.Railway:Toei.Shinjuku", 
-                ] 
-        #self.get_logger().info('Filtered Train Info Publisher has started.')
+            "odpt.Railway:Toei.Shinjuku",  # 都営新宿線
+            "odpt.Railway:Toei.Oedo",     # 都営大江戸線
+            "odpt.Railway:Toei.Asakusa",  # 都営浅草線
+        ]
 
     def timer_callback(self):
         try:
@@ -36,12 +42,12 @@ class FilteredTrainInfoPublisher(Node):
                     msg = String()
                     status = train_info.get("odpt:trainInformationStatus", "情報なし")
                     text = train_info.get("odpt:trainInformationText", {}).get("ja", "詳細なし")
-                    
+
                     railway_name = railway.replace("odpt.Railway:", "")
                     msg.data = f"[{railway_name}] 状況:{text} 詳細:{status}"
                     self.publisher_.publish(msg)
-                    #self.get_logger().info(f'Published: {msg.data}')
         except Exception as e:
+           # self.get_logger().error(f"Failed to fetch train info: {e}")
             sys.exit(1)
 
 
@@ -53,5 +59,7 @@ def main(args=None):
     publisher.destroy_node()
     rclpy.shutdown()
 
+
 if __name__ == '__main__':
     main()
+
