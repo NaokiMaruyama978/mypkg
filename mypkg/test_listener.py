ecf1c4a3f4ed43f490b529d8e487f0949a83bb50
NaokiMaruyama978 <s23c1135vq@s.chibakoudai.jp>
2025-01-07 02:54:10 +0900

    Update test_listener.py

--- mypkg/test_listener.py
+++ mypkg/test_listener.py
@@ -2,25 +2,29 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
-class YearZodiacListener(Node):
+class TrainInfoListener(Node):
     def __init__(self):
-        super().__init__('year_zodiac_listener')
-        self.subscription = self.create_subscription(String,'/zodiac',self.listener_callback,10)
+        super().__init__('train_info_listener')
+        self.subscription = self.create_subscription(
+            String,
+            'train_delay_info',  # パブリッシャーと同じトピック名
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.get_logger().info('Train Info Listener has started.')
 
     def listener_callback(self, msg):
-        self.get_logger().info(f"受信:{msg.data}")
+        self.get_logger().info(f'Received: {msg.data}')
 
 def main(args=None):
     rclpy.init(args=args)
-    node = YearZodiacListener()
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
+    listener = TrainInfoListener()
+    rclpy.spin(listener)
+
+    listener.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
+
 
