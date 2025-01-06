7054af0b157f98e8b5518cee9030723b9f8acc3b
Naoki maruyama <s23c1135vq@s.chibakoudai.jp>
2025-01-07 02:21:03 +0900

    電車の遅延情報教えてくれるプログラムにした

--- mypkg/zodiac_publisher.py
+++ mypkg/zodiac_publisher.py
@@ -8,13 +8,12 @@ class FilteredTrainInfoPublisher(Node):
         super().__init__('filtered_train_info_publisher')
         self.publisher_ = self.create_publisher(String, 'train_delay_info', 10)
         self.timer = self.create_timer(10.0, self.timer_callback)  # 60秒ごとに更新
-        self.api_url = "https://api-public.odpt.org/api/v4/odpt:TrainInformation?odpt:operator=odpt.Operator:Toei"
-        self.api_key = "3o7usx306xa0q9chlckckk2xxm2jvthznu0vnk3fktuu9gdirfp3pzcecwlpagwa"  # 取得したAPIキーを設定
+        self.api_url = "https://api.odpt.org/api/v4/odpt:TrainInformation"
+        self.api_key = "3o7usx306xa0q9chlckckk2xxm2jvthznu0vnk3fktuu9gdirfp3pzcecwlpagwa"  # 取得したAPIキーを設定 
         self.target_railways = [
-            "odpt.Railway:Toei.Oedo", 
+ 
             "odpt.Railway:Toei.Shinjuku", 
-            "odpt.Railway:Toei.Asakusa"
-        ]
+                ] 
         self.get_logger().info('Filtered Train Info Publisher has started.')
 
     def timer_callback(self):
@@ -33,7 +32,7 @@ class FilteredTrainInfoPublisher(Node):
                     status = train_info.get("odpt:trainInformationStatus", "情報なし")
                     text = train_info.get("odpt:trainInformationText", {}).get("ja", "詳細なし")
                     
-                    msg.data = f"路線: {railway}, 状況: {status}, 詳細: {text}"
+                    msg.data = f"[{railway}], 状況: {text}, 詳細: {status}"
                     self.publisher_.publish(msg)
                     self.get_logger().info(f'Published: {msg.data}')
         except Exception as e:
@@ -49,4 +48,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
