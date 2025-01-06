0cb26456341445ee7f48acd6cbb12efab35ee91b
Naoki maruyama <s23c1135vq@s.chibakoudai.jp>
2025-01-07 02:23:11 +0900

    テスト書き直した

--- test/test.bash
+++ test/test.bash
@@ -9,9 +9,9 @@ source $dir/.bashrc
 
 # タイムアウト時間を設定
 #timeout 10 ros2 launch mypkg zodiac_publisher.launch.py > /tmp/mypkg.log
-timeout 10 ros2 launch mypkg zodiac_publisher.launch.py | tee - /tmp/mypkg.log
+timeout 12 ros2 launch mypkg zodiac_publisher.launch.py | tee - /tmp/mypkg.log
 
 # ログファイルの内容を検索
 #cat /tmp/mypkg.log | grep '年:2028 干支:申(さる)'
-cat /tmp/mypkg.log | grep '年:'
-cat /tmp/mypkg.log | grep '干支:'
+cat /tmp/mypkg.log | grep '[Toei.Shinjuku]'
+#cat /tmp/mypkg.log | grep '干支:'
